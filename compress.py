175f0fe64747128fe5bcfbf881919f2200d68295
Bebroros <retrosab@gmail.com>
2024-10-23 22:24:56 +0300

    Fixed problem with wrong compressing via gzip. Fixed problem with overriding .gz archives. Added relative way to zip archive. Added possible extension request in compressing to zip. Added control of user actions

--- compress.py
+++ compress.py
@@ -8,27 +8,30 @@ def generate_archive_name(filename, outdir) -> str:
     counter = 1
     while True:
         name = f"{filename}_{datetime.date.today().strftime('%Y%m%d')}_{counter}"
-        if not Path(f"{outdir}/{name}.zip").exists() or Path(f"{outdir}/{name}.gzip").exists():
-            print(outdir/name)
+        if Path(f"{outdir}/{name}.zip").exists() or Path(f"{outdir}/{name}.gz").exists():
+            counter += 1
+        else:
+            print(outdir / name)
             return name
-        counter += 1
 
 
-def compress_to_zip(indir, outdir, filename) -> None:
-    with zipfile.ZipFile(outdir/f'{filename}.zip', 'w', compression=zipfile.ZIP_DEFLATED) as zip_file:
+def compress_to_zip(indir, outdir, filename, extension='*') -> None:
+    with zipfile.ZipFile(outdir / f'{filename}.zip', 'w', compression=zipfile.ZIP_DEFLATED) as zip_file:
         if indir.is_dir():
-            list_of_files = [file for file in indir.rglob('*') if file.is_file() and file != outdir/f'{filename}.zip']
+            list_of_files = [file for file in indir.rglob(f'{extension}') if file.is_file()
+                             and file != outdir / f'{filename}.zip']
             for file in list_of_files:
-                zip_file.write(file, file.name)
+                zip_file.write(file, file.relative_to(indir))
         else:
             zip_file.write(indir, indir.name)
     print("Zip file created!")
 
 
 def compress_to_gzip(indir, outdir, filename) -> None:
-    print(filename)
-    with gzip.open(outdir/f'{filename}.gzip', 'wt') as gzip_file:
-        gzip_file.write(str(indir))
+    with open(indir, 'rb') as file:
+        data = file.read()
+        with gzip.open(outdir / f'{filename}.gz', 'wb') as gzip_file:
+            gzip_file.write(data)
     print("Gzip file created!")
 
 
@@ -46,13 +49,38 @@ def ask_users_directory():
     return indir, outdir
 
 
+def user_action():
+    while True:
+        action = input("What do you want to do? (C)ompress, (Q)uit:")
+        if action.lower() == "q":
+            print("Thank you!")
+            input("Press enter to quit")
+            return quit()
+        elif action.lower() == "c":
+            type_of_archive = input("What type of archive do you want to compress(zip, gzip)?")
+            if type_of_archive.lower() == "zip":
+                return 'zip'
+            elif type_of_archive.lower() == "gzip":
+                return 'gzip'
+            else:
+                print("Invalid input. Please try again.")
+                pass
+        else:
+            print("Invalid input. Please try again.")
+            pass
+
+
 def main():
-    #indir, outdir = ask_users_directory()
-    indir = Path(r'C:\Users\retro\PycharmProjects\pythonProject2\bebra\birthday.txt')
-    outdir = Path(r'C:\Users\retro\PycharmProjects\pythonProject2\bebra')
-    filename = input("Enter name of archive: ")
-    compress_to_zip(indir, outdir, generate_archive_name(filename, outdir))
-    compress_to_gzip(indir, outdir, generate_archive_name(filename, outdir))
+    while True:
+        action = user_action()
+        #indir, outdir = ask_users_directory()
+        indir = Path(r'C:\Users\retro\PycharmProjects\pythonProject2\bebra')
+        outdir = Path(r'C:\Users\retro\PycharmProjects\pythonProject2\bebra')
+        filename = input("Enter name of archive: ")
+        if action == 'zip':
+            compress_to_zip(indir, outdir, generate_archive_name(filename, outdir))
+        elif action == 'gzip':
+            compress_to_gzip(indir, outdir, generate_archive_name(filename, outdir))
 
 
 if __name__ == '__main__':
