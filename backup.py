84615ef72a21e787a83fb356a6a4b640178bed88
Bebroros <retrosab@gmail.com>
2024-10-27 01:02:50 +0300

    added argparse

--- backup.py
+++ backup.py
@@ -1,3 +1,6 @@
+import argparse
+import sys
+from pathlib import Path
 import compress
 import decompress
 
@@ -11,6 +14,45 @@ def get_extension(indir):
         return extensions
     else:
         return None
+
+
+def arg_parser():
+    parser = argparse.ArgumentParser("Compress or decompress any file")
+    action_group = parser.add_mutually_exclusive_group()
+    action_group.add_argument('-c', '--compress',
+                              help="Compress a file",
+                              dest='compress',
+                              action='store_true')
+    action_group.add_argument('-d', '--decompress',
+                              help="Decompress a file",
+                              dest='decompress',
+                              action='store_true')
+    compressing_group = parser.add_mutually_exclusive_group()
+    compressing_group.add_argument('-z', '--zip',
+                                   help="Compress a zip file",
+                                   dest='zip',
+                                   action='store_true')
+    compressing_group.add_argument('-g', '--gzip',
+                                   help="Create a gzip file",
+                                   dest='gzip',
+                                   action='store_true')
+    compressing_group.add_argument('-b', '--bzip2',
+                                   help="Create a bzip2 file",
+                                   dest='bzip2',
+                                   action='store_true')
+    compressing_group.add_argument('-x', '--xzip',
+                                   help="Create a xzip file",
+                                   dest='xzip',
+                                   action='store_true')
+    parser.add_argument('-i', '--indir',
+                        help="Input directory",
+                        dest='indir',
+                        required=True)
+    parser.add_argument('-o', '--outdir',
+                        help="Output directory",
+                        dest='outdir',
+                        required=True)
+    return parser.parse_args()
 
 
 def user_action():
@@ -73,7 +115,41 @@ def user_action():
 
 
 def main():
-    user_action()
+    if len(sys.argv) > 1:
+        args = arg_parser()
+        if args.compress:
+            if args.zip:
+                compress.compress_to_zip(
+                    Path(args.indir), Path(args.outdir),
+                    compress.generate_archive_name(Path(args.indir), Path(args.outdir)),
+                    get_extension(Path(args.indir))
+                )
+            elif args.gzip:
+                compress.compress_to_gzip(
+                    Path(args.indir), Path(args.outdir),
+                    compress.generate_archive_name(Path(args.indir), Path(args.outdir)),
+                )
+            elif args.bzip2:
+                compress.compress_to_bzip2(
+                    Path(args.indir), Path(args.outdir),
+                    compress.generate_archive_name(Path(args.indir), Path(args.outdir))
+                )
+            elif args.xzip:
+                compress.compress_to_xz(
+                    Path(args.indir), Path(args.outdir),
+                    compress.generate_archive_name(Path(args.indir), Path(args.outdir))
+                )
+        elif args.decompress:
+            if args.zip:
+                decompress.decompress_zip(Path(args.indir), Path(args.outdir))
+            if args.gzip:
+                decompress.decompress_gzip(Path(args.indir), Path(args.outdir))
+            if args.bzip2:
+                decompress.decompress_bzip2(Path(args.indir), Path(args.outdir))
+            if args.xzip:
+                decompress.decompress_xz(Path(args.indir), Path(args.outdir))
+    else:
+        user_action()
 
 
 if __name__ == "__main__":
